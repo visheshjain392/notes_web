b0d5e08515dd211a8fd5e4c8a26c0897d7d15df4
visheshjain392 <145132184+visheshjain392@users.noreply.github.com>
2025-04-06 22:00:45 +0530

    Update index.js

--- Note-app/backend/index.js
+++ Note-app/backend/index.js
@@ -20,7 +20,7 @@ const app = express();
 // to make input as json
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin: ["http://localhost:5173"], credentials: true}));
+app.use(cors({origin: ["https://notes-web-ruddy.vercel.app"], credentials: true}));
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
